183304ef50e442fbb0af7b4c05621672ae7b14bc
Valentina Gil <valentinagilantia@gmail.com>
2023-09-19 19:05:22 -0500

    Footer

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,14 +1,85 @@
 import React from "react"; // rafce
 import "../css/main.css";
+import logo from "../img/logo.svg"; 
+import facebook from "../img/icon-facebook.svg"; 
+import twitter from "../img/icon-twitter.svg"; 
+import pinterest from "../img/icon-pinterest.svg"; 
+import instagram from "../img/icon-instagram.svg"; 
 
 const Footer = () => {
     return (
         <>
-            {   
-                <footer>
-                    <h1>Footer</h1>
-                </footer>
-            }
+ <footer class="footer">
+    <div class="footer-columna1">
+    <img class="logo" src={logo} alt="Logo" />
+        
+      <nav className="nav footer-nav-1">  
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+              About
+            </a>
+            <span className="nav__bar "></span>
+          </li>
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+              Careers
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+              Events
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+              Products
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link"></a>
+            <span className="nav__bar"></span>
+          </li>   
+      </nav>
+      </div>
+
+      <div class="footer-columna2">
+      <nav className="nav footer-nav2">  
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+            <img src={facebook} alt="Facebook"/>
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+            <img src={twitter}  alt="Twitter"/>
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item footer-nav-item">
+            <a href="#" className="nav__link">
+            <img src={pinterest} alt="Pinterest"/>
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item ">
+            <a href="#" className="nav__link">
+            <img src={instagram} alt="Instagram"/>
+            </a>
+            <span className="nav__bar"></span>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link"></a>
+            <span className="nav__bar"></span>
+          </li>
+      </nav>   
+      <br />
+      <p class="footer-rights">© 2021 Loopstudios. All rights reserved.</p>
+      </div>
+  </footer>
         </>
     );
 };
